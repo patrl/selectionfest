5714df4d63d25ca8ccac0b94aaef4d03b2ce7b42
Patrick Elliott <patrick.d.elliott@gmail.com>
2017-05-08 16:43:42 +0200

    cleanup

--- site.hs
+++ site.hs
@@ -19,24 +19,11 @@ main = hakyllWith config $ do
         route   idRoute
         compile compressCssCompiler
 
-    match "./*.org" $ do
-    route  $ setExtension "html"
+    match "*.org" $ do
+      route  $ setExtension "html"
       compile $ pandocCompiler
               >>= loadAndApplyTemplate "templates/default.html" defaultContext
               >>= relativizeUrls
-
-    -- match (fromList ["about.rst", "contact.markdown"]) $ do
-    --     route   $ setExtension "html"
-    --     compile $ pandocCompiler
-    --         >>= loadAndApplyTemplate "templates/default.html" defaultContext
-    --         >>= relativizeUrls
-
-    -- match "posts/*" $ do
-    --     route $ setExtension "html"
-    --     compile $ pandocCompiler
-    --         >>= loadAndApplyTemplate "templates/post.html"    postCtx
-    --         >>= loadAndApplyTemplate "templates/default.html" postCtx
-    --         >>= relativizeUrls
 
     -- create ["archive.html"] $ do
     --     route idRoute
@@ -51,21 +38,6 @@ main = hakyllWith config $ do
     --             >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
     --             >>= loadAndApplyTemplate "templates/default.html" archiveCtx
     --             >>= relativizeUrls
-
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            -- posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    -- listField "posts" postCtx (return posts) `mappend`
-                    -- constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
