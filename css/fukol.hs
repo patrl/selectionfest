d1940253ea0c8335ce57b89e2b9ab649bc384575
Patrick Elliott <patrick.d.elliott@gmail.com>
2017-05-08 16:29:49 +0200

    Added clay test.

--- css/fukol.hs
+++ css/fukol.hs
@@ -2,7 +2,21 @@
 import           Clay
 
 test :: Css
-test = ...
+test = body ?
+       do background black
+          color      green
+          border     dashed (px 2) yellow
 
 main :: IO ()
 main = putCss test
+
+-- .fukol-grid {
+--   display: flex; /* 1 */
+--   flex-wrap: wrap; /* 2 */
+--   margin: -0.5em; /* 5 (edit me!) */
+-- }
+
+-- .fukol-grid > * {
+--   flex: 1 0 5em; /* 3 (edit me!) */
+--   margin: 0.5em; /* 4 (edit me!) */
+-- }
