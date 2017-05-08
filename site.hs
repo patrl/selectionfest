cc74ab800344d13273ec09324bd5d8da8d777bc2
Patrick Elliott <patrick.d.elliott@gmail.com>
2017-05-08 15:48:27 +0200

    Tried adding org file conversion

--- site.hs
+++ site.hs
@@ -15,6 +15,13 @@ main = hakyllWith config $ do
         route   idRoute
         compile compressCssCompiler
 
+    match "about.org" $ do
+      route  $ setExtension "html"
+      compile $ pandocCompiler
+              >>= loadAndApplyTemplate "templates/default.html"
+              >>defaultContext
+              >>= relativizeUrls
+
     -- match (fromList ["about.rst", "contact.markdown"]) $ do
     --     route   $ setExtension "html"
     --     compile $ pandocCompiler
